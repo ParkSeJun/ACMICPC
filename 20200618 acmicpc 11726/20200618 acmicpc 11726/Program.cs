1c33af8a1b8a66d430463b2e9efa8faf72f383d2
ParkSeJun <chunsa1166@naver.com>
2020-06-18 17:03:12 +0900

    초안 (맵 없이)

--- 20200618 acmicpc 11726/20200618 acmicpc 11726/Program.cs
+++ 20200618 acmicpc 11726/20200618 acmicpc 11726/Program.cs
@@ -10,6 +10,20 @@ namespace _20200618_acmicpc_11726
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(Solve(1));
+            Console.WriteLine(Solve(2));
+            Console.WriteLine(Solve(3));
+            Console.WriteLine(Solve(4));
+        }
+
+        public static int Solve(int n)
+        {
+            if (n < 0)
+                return 0;
+            if (n <= 1)
+                return 1;
+
+            return Solve(n - 1) + Solve(n - 2);
         }
     }
 }
