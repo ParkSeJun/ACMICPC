e9e874c40c5c711e5572de09b63b6f41795b256f
ParkSeJun <chunsa1166@naver.com>
2020-06-23 14:44:55 +0900

    1149 정답

--- 20200618 acmicpc 1149/Program.cs
+++ 20200618 acmicpc 1149/Program.cs
@@ -11,10 +11,39 @@ namespace _20200618_acmicpc_1149
         static void Main(string[] args)
         {
             int count = int.Parse(Console.ReadLine().Trim());
+            int[] minRGB = new int[3] { 0, 0, 0 };
             for (int i = 0; i < count; i++)
             {
+                var input = Console.ReadLine().Trim().Split(' ');
+                int[] RGB = new int[3] { int.Parse(input[0]), int.Parse(input[1]), int.Parse(input[2]) };
 
+                if (i == 0)
+                {
+                    for (int j = 0; j < 3; ++j)
+                        minRGB[j] = RGB[j];
+                    continue;
+                }
+
+                int[] newMinRGB = new int[3];
+                for (int j = 0; j < 3; ++j)
+                {
+                    int min = int.MaxValue;
+                    for (int k = 0; k < 3; ++k)
+                    {
+                        if (j == k)
+                            continue;
+
+                        if (minRGB[k] < min)
+                            min = minRGB[k];
+                    }
+                    newMinRGB[j] = min + RGB[j];
+                }
+
+                for (int j = 0; j < 3; ++j)
+                    minRGB[j] = newMinRGB[j];
             }
+
+            Console.WriteLine(Math.Min(Math.Min(minRGB[0], minRGB[1]), minRGB[2]));
         }
     }
 }
