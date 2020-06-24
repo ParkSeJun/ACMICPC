6772b06d938cdf781147ecb739657e7e4ca0cfe3
ParkSeJun <chunsa1166@naver.com>
2020-06-24 14:11:03 +0900

    2193 https://www.acmicpc.net/problem/2193

---
+++ 20200624 2193/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20200624_2193
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
