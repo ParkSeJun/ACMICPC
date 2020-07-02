6e9b8b08e0bd168d4bfe8b573746ccbb11051644
ParkSeJun <chunsa1166@naver.com>
2020-07-02 11:48:45 +0900

    13458 시험 감독

---
+++ 20200702 13458/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20200702_13458
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            List<int> testerCount = new List<int>();
+            var testers = Console.ReadLine().Split(' ');
+            for (int i = 0; i < count; i++)
+            {
+                testerCount.Add(int.Parse(testers[i]));
+            }
+            var viewers = Console.ReadLine().Split(' ');
+            int mainViewCount = int.Parse(viewers[0]);
+            int subViewCount = int.Parse(viewers[1]);
+        }
+    }
+}
