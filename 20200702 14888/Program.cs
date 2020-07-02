0403b05eb1d115d2733516f1d665a0c211d56060
ParkSeJun <chunsa1166@naver.com>
2020-07-02 19:00:03 +0900

    중간 커밋

--- 20200702 14888/Program.cs
+++ 20200702 14888/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +9,41 @@ namespace _20200702_14888
 {
     class Program
     {
+        public class Key 
+        {
+            public int index;
+            public int leftAdd;
+            public int leftSub;
+            public int leftMul;
+            public int leftDiv;
+
+            public override bool Equals(object obj)
+            {
+                Key other = obj as Key;
+                if (other == null)
+                    return false;
+
+                return index == other.index && leftAdd == other.leftAdd && leftSub == other.leftSub && leftMul == other.leftMul && leftDiv == other.leftDiv;
+            }
+        }
+
+        public class Value
+        {
+            public int min;
+            public int max;
+
+            public override bool Equals(object obj)
+            {
+                Value other = obj as Value;
+                if (other == null)
+                    return false;
+
+                return min == other.min && max == other.max;
+            }
+        }
+
+        static Dictionary<Key, 
+
         static void Main(string[] args)
         {
         }
