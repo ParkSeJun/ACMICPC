67e7b909de4e1712747c6ba524a03f93aeb99dbf
ParkSeJun <chunsa1166@naver.com>
2020-07-07 11:52:11 +0900

    15683 정답

--- 20200706 15683/Program.cs
+++ 20200706 15683/Program.cs
@@ -1,11 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.Design.Serialization;
-using System.Data;
 using System.Linq;
-using System.Security.Cryptography;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _20200706_15683
 {
@@ -85,6 +80,7 @@ namespace _20200706_15683
             }
 
             int a = Solve(tvList, 0);
+            Console.WriteLine(a);
         }
 
         static int Solve(List<TV> tvList, int index)
@@ -95,20 +91,59 @@ namespace _20200706_15683
                 return ret;
             }
 
-            tvList
+            tvList = GetClone(tvList);
 
-            int a, b, c, d = 0;
+            int a = int.MaxValue, b = int.MaxValue, c = int.MaxValue, d = int.MaxValue;
             switch (tvList[index].type)
             {
                 case 1:
-                    
+                    tvList[index].dir = DIR.UP;
+                    a = Solve(tvList, index + 1);
+                    tvList[index].dir = DIR.LEFT;
+                    b = Solve(tvList, index + 1);
+                    tvList[index].dir = DIR.DOWN;
+                    c = Solve(tvList, index + 1);
+                    tvList[index].dir = DIR.RIGHT;
+                    d = Solve(tvList, index + 1);
+                    break;
+                case 2:
+                    tvList[index].dir = DIR.UP | DIR.DOWN;
+                    a = Solve(tvList, index + 1);
+                    tvList[index].dir = DIR.LEFT | DIR.RIGHT;
+                    b = Solve(tvList, index + 1);
+                    break;
+                case 3:
+                    tvList[index].dir = DIR.UP | DIR.LEFT;
+                    a = Solve(tvList, index + 1);
+                    tvList[index].dir = DIR.LEFT | DIR.DOWN;
+                    b = Solve(tvList, index + 1);
+                    tvList[index].dir = DIR.RIGHT | DIR.DOWN;
+                    c = Solve(tvList, index + 1);
+                    tvList[index].dir = DIR.RIGHT | DIR.UP;
+                    d = Solve(tvList, index + 1);
+                    break;
+                case 4:
+                    tvList[index].dir = DIR.LEFT | DIR.UP | DIR.DOWN;
+                    a = Solve(tvList, index + 1);
+                    tvList[index].dir = DIR.LEFT | DIR.UP | DIR.RIGHT;
+                    b = Solve(tvList, index + 1);
+                    tvList[index].dir = DIR.RIGHT | DIR.UP | DIR.DOWN;
+                    c = Solve(tvList, index + 1);
+                    tvList[index].dir = DIR.LEFT | DIR.RIGHT | DIR.DOWN;
+                    d = Solve(tvList, index + 1);
+                    break;
+                case 5:
+                    tvList[index].dir = DIR.LEFT | DIR.UP | DIR.RIGHT | DIR.DOWN;
+                    a = Solve(tvList, index + 1);
                     break;
             }
+
+            return Math.Min(a, Math.Min(b, Math.Min(c, d)));
         }
 
         static List<TV> GetClone(List<TV> tvList)
         {
-            from e in tvList let e.p = new Point(e.p.y, e.p.x) 
+            return (from e in tvList select new TV(e.type, new Point(e.p.y, e.p.x), e.dir)).ToList();
         }
 
         static int GetBlindSpotCount(List<TV> tvList)
@@ -131,28 +166,28 @@ namespace _20200706_15683
                 {
                     nearWall = wallList.Where(e => e.x == tv.p.x && e.y < tv.p.y).OrderByDescending(e => e.y).Take(1).SingleOrDefault();
                     t = nearWall == null ? 0 : nearWall.y;
-                    for (int i = tv.p.y; i <= t; i--)
+                    for (int i = tv.p.y; i >= t; i--)
                         arr[i][tv.p.x] = true;
                 }
                 if (tv.dir.HasEnumFlag(DIR.LEFT))
                 {
                     nearWall = wallList.Where(e => e.y == tv.p.y && e.x < tv.p.x).OrderByDescending(e => e.x).Take(1).SingleOrDefault();
                     t = nearWall == null ? 0 : nearWall.x;
-                    for (int i = tv.p.x; i <= t; i--)
+                    for (int i = tv.p.x; i >= t; i--)
                         arr[tv.p.y][i] = true;
                 }
                 if (tv.dir.HasEnumFlag(DIR.DOWN))
                 {
                     nearWall = wallList.Where(e => e.x == tv.p.x && e.y > tv.p.y).OrderBy(e => e.y).Take(1).SingleOrDefault();
                     t = nearWall == null ? h - 1 : nearWall.y;
-                    for (int i = tv.p.y; i >= t; i++)
+                    for (int i = tv.p.y; i <= t; i++)
                         arr[i][tv.p.x] = true;
                 }
                 if (tv.dir.HasEnumFlag(DIR.RIGHT))
                 {
                     nearWall = wallList.Where(e => e.y == tv.p.y && e.x > tv.p.x).OrderBy(e => e.x).Take(1).SingleOrDefault();
                     t = nearWall == null ? w - 1 : nearWall.x;
-                    for (int i = tv.p.x; i >= t; i++)
+                    for (int i = tv.p.x; i <= t; i++)
                         arr[tv.p.y][i] = true;
                 }
             }
@@ -175,6 +210,11 @@ namespace _20200706_15683
         public static int ToInt(this Program.DIR dir)
         {
             return Convert.ToInt32(dir);
+        }
+
+        public static Program.DIR ToEnum(this int n)
+        {
+            return (Program.DIR)Enum.ToObject(typeof(Program.DIR), n);
         }
 
         public static bool HasEnumFlag(this Program.DIR dir, Program.DIR flag)
