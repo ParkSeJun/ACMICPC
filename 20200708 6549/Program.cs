4bb9a45f66eadc9f72a15efac9130a38aafa5e56
ParkSeJun <chunsa1166@naver.com>
2020-07-08 18:53:44 +0900

    6549 나중에 도전 .. (세그먼트트리)

--- 20200708 6549/Program.cs
+++ 20200708 6549/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _20200708_6549
 {
@@ -10,6 +7,48 @@ namespace _20200708_6549
     {
         static void Main(string[] args)
         {
+            while(true)
+            {
+                var row = Console.ReadLine().Split(' ');
+                int count = int.Parse(row[0]);
+                if (count == 0)
+                    break;
+
+                List<long> list = new List<long>();
+                for (int i = 0; i < count; i++)
+                    list.Add(int.Parse(row[i + 1]));
+
+                long value = Solve(list);
+                Console.WriteLine(value);
+            }
+        }
+
+        static long Solve(List<long> list)
+        {
+            long max = 0;
+            for (int i = 0; i < list.Count; i++)
+            {
+                int leftMostIndex = i;
+                int rightMostIndex = i;
+
+                for (int j = i - 1; j >= 0; j--)
+                {
+                    if (list[j] < list[i])
+                        break;
+                    leftMostIndex = j;
+                }
+                for (int j = i + 1; j < list.Count; j++)
+                {
+                    if (list[j] < list[i])
+                        break;
+                    rightMostIndex = j;
+                }
+
+                long thisValue = list[i] * (rightMostIndex - leftMostIndex + 1);
+                if (max < thisValue)
+                    max = thisValue;
+            }
+            return max;
         }
     }
 }
