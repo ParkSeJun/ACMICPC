32acf14fd560db119133c714924d8ce8c98518f7
ParkSeJun <chunsa1166@naver.com>
2020-07-08 18:54:03 +0900

    15649 N과 M (1) 성공.

---
+++ 20200708 15649/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20200708_15649
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, m;
+            var row = Console.ReadLine().Split(' ');
+            n = int.Parse(row[0]);
+            m = int.Parse(row[1]);
+            Solve(n, m, new List<int>());
+        }
+
+        static void Solve(int n, int m, List<int> l)
+        {
+            l = new List<int>(l);
+            if (l.Count == m)
+            {
+                Console.WriteLine(string.Join(" ", l));
+                return;
+            }
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (l.Contains(i))
+                    continue;
+                l.Add(i);
+                Solve(n, m, l);
+                l.Remove(i);
+            }
+        }
+    }
+}
