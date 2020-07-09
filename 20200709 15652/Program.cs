2a80fa4a1b35e5d3829bdf8ae7414d8eb69eb1a2
ParkSeJun <chunsa1166@naver.com>
2020-07-09 11:33:39 +0900

    15652 N과 M (4) 정답.

---
+++ 20200709 15652/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20200709_15652
+{
+    class Program
+    {
+
+        static StringBuilder sb = new StringBuilder();
+        static void Main(string[] args)
+        {
+            var row = Console.ReadLine().Split(' ').Select(e => int.Parse(e)).ToList();
+            int n = row[0];
+            int m = row[1];
+
+            Solve(n, m, new List<int>());
+            Console.WriteLine(sb.ToString());
+        }
+
+        static void Solve(int n, int m, List<int> l)
+        {
+            if (l.Count >= m)
+            {
+                sb.AppendLine(string.Join(" ", l));
+                return;
+            }
+
+            l = new List<int>(l);
+            for (int i = 1; i <= n; i++)
+            {
+                if (l.Count > 0 && l[l.Count - 1] > i)
+                    continue;
+
+                l.Add(i);
+                Solve(n, m, l);
+                l.RemoveAt(l.Count - 1);
+            }
+        }
+    }
+}
