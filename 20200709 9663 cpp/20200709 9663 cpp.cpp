d25fc17662487aae95fda43d9565e2a2bb788e0f
ParkSeJun <chunsa1166@naver.com>
2020-07-09 14:29:51 +0900

    9663 N Queen C++로 정답. (C#은 시간초과)

---
+++ 20200709 9663 cpp/20200709 9663 cpp.cpp
@@ -0,0 +1,53 @@
+﻿#include <stdio.h>
+
+int count = 0;
+int* arr;
+int index=0;
+int n;
+
+bool IsValid(int x)
+{
+	for (int i = 0; i < index; i++)
+	{
+		if (arr[i] == x)
+			return false;
+
+		int xDiff = arr[i] - x;
+		if (xDiff < 0)
+			xDiff = -xDiff;
+
+		if (index - i == xDiff)
+			return false;
+	}
+	return true;
+}
+
+void Solve()
+{
+	if (index == n)
+	{
+		count++;
+		return;
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		if (!IsValid(i))
+			continue;
+
+		arr[index++] = i;
+		Solve();
+		index--;
+	}
+}
+
+int main()
+{
+	scanf("%d", &n);
+	arr = new int[n];
+
+	Solve();
+	printf("%d", count);
+
+	return 0;
+}
---
+++ 20200709 9663/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20200709_9663
+{
+    class Program
+    {
+        static int count = 0;
+        static int[] arr;
+        static int index;
+        static int n;
+
+        static void Main(string[] args)
+        {
+            n = int.Parse(Console.ReadLine());
+            //n = 15;
+
+            arr = new int[n];
+            index = 0;
+
+            Solve();
+            Console.WriteLine(count);
+        }
+
+        static void Solve()
+        {
+            if (index == n)
+            {
+                count++;
+                return;
+            }
+
+            for (int j = 0; j < n; j++)
+            {
+                if (!IsValid(j))
+                    continue;
+
+                arr[index++] = j;
+                Solve();
+                index--;
+            }
+        }
+
+        static bool IsValid(int x)
+        {
+            for (int i = 0; i < index; ++i)
+            {
+                if (arr[i] == x)
+                    return false;
+
+                if (index - i == Math.Abs(arr[i] - x))
+                    return false;
+            }
+            return true;
+        }
+    }
+}
