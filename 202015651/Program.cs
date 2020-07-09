c1a7efc121620bcebf55d5f2dde2253c9568f482
ParkSeJun <chunsa1166@naver.com>
2020-07-09 11:03:56 +0900

    20200709 15651 N과 M (3) 정답

---
+++ 202015651/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _202015651
+{
+    class Program
+    {
+        static StringBuilder sb = new StringBuilder();
+        static void Main(string[] args)
+        {
+            var row = Console.ReadLine().Split(' ').Select(e => int.Parse(e)).ToList();
+            int n = row[0];
+            int m = row[1];
+
+            //int n = 7, m = 7;
+
+            Solve(n, m, new List<int>());
+            Console.WriteLine(sb.ToString());
+        }
+
+        static void Solve(int n, int m, List<int> l)
+        {
+            if (l.Count >= m)
+            {
+                sb.AppendLine(string.Join(" ", l));
+                return;
+            }
+
+            l = new List<int>(l);
+            for (int i = 1; i <= n; ++i)
+            {
+                l.Add(i);
+                Solve(n, m, l);
+                l.RemoveAt(l.Count-1);
+            }
+        }
+    }
+}
