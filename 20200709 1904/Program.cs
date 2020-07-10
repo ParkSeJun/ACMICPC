c1fd0f10f5a247b45085f7e5b736b081ba30e872
ParkSeJun <chunsa1166@naver.com>
2020-07-10 12:34:53 +0900

    1904 01타일 정답

---
+++ 20200709 1904/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _20200709_1904
+{
+    class Program
+    {
+        static Dictionary<int, int> table = new Dictionary<int, int>();
+
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine(Solve(a));
+        }
+
+        static int Solve(int n)
+        {
+            if (n < 0)
+                return 0;
+            if (n <= 1)
+                return 1;
+
+            if (!table.ContainsKey(n))
+            {
+                table[n] = (Solve(n - 1) + Solve(n - 2)) % 15746;
+            }
+            return table[n];
+        }
+    }
+}
