19f214fc8b46f8b302e6f88c5360ca6916d320b8
ParkSeJun <chunsa1166@naver.com>
2020-07-10 16:34:53 +0900

    9461 파도반 수열

---
+++ 20200710 9461/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _20200710_9461
+{
+    class Program
+    {
+        static Dictionary<int, int> table = new Dictionary<int, int>();
+
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            for (int i = 0; i < count; i++)
+            {
+                int n = int.Parse(Console.ReadLine());
+                Console.WriteLine(Solve(n));
+            }
+        }
+
+        static int Solve(int n)
+        {
+            if (n == -1 || n == 1)
+                return 1;
+            if (n <= 0)
+                return 0;
+
+            if (!table.ContainsKey(n))
+            {
+                table[n] = Solve(n - 1) + Solve(n - 5);
+            }
+            return table[n];
+        }
+    }
+}
