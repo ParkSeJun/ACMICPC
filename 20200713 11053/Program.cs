c7ea6617da62c6fcfd5f590a766c3cb03a04f546
ParkSeJun <chunsa1166@naver.com>
2020-07-13 15:43:29 +0900

    11053 정답.

--- 20200713 11053/Program.cs
+++ 20200713 11053/Program.cs
@@ -10,6 +10,26 @@ namespace _20200713_11053
     {
         static void Main(string[] args)
         {
+            int count = int.Parse(Console.ReadLine());
+            int[] arr = new int[count];
+            int[] table = new int[count];
+            var row = Console.ReadLine().Split(' ');
+            for (int i = 0; i < count; i++)
+            {
+                arr[i] = int.Parse(row[i]);
+                table[i] = 0;
+            }
+
+            for (int i = 0; i < count; i++)
+            {
+                int max = 0;
+                for (int j = 0; j < i; j++)
+                    if (arr[j] < arr[i] && max < table[j])
+                        max = table[j];
+                table[i] = max + 1;
+            }
+
+            Console.WriteLine(table.Max());
         }
     }
 }
