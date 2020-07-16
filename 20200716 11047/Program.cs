c2461a29e2be368481eb4dcfa7f6dfad2276d500
ParkSeJun <chunsa1166@naver.com>
2020-07-16 11:05:01 +0900

    11047 정답.

--- 20200716 11047/Program.cs
+++ 20200716 11047/Program.cs
@@ -1,8 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
+/*
+10 4200
+1
+5
+10
+50
+100
+500
+1000
+5000
+10000
+50000
+*/
 
 namespace _20200716_11047
 {
@@ -10,6 +20,26 @@ namespace _20200716_11047
     {
         static void Main(string[] args)
         {
+            var row = Array.ConvertAll(Console.ReadLine().Split(' '), e => int.Parse(e));
+            int count = row[0];
+            int dest = row[1];
+            int[] arr = new int[count];
+            for (int i = 0; i < count; i++)
+                arr[i] = int.Parse(Console.ReadLine());
+
+            int cur = dest;
+            int coinCount = 0;
+            for (int i = 0; i < count; i++)
+            {
+                if (cur >= arr[count - 1 - i])
+                {
+                    int div = cur / arr[count - 1 - i];
+                    coinCount += div;
+                    cur -= arr[count - 1 - i] * div;
+                }
+            }
+
+            Console.WriteLine(coinCount);
         }
     }
 }
