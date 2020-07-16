b865d3ef3f5978127330eb808e71582ea1355bf3
ParkSeJun <chunsa1166@naver.com>
2020-07-16 11:22:58 +0900

    1931 정답

--- 20200716 1931/Program.cs
+++ 20200716 1931/Program.cs
@@ -8,8 +8,41 @@ namespace _20200716_1931
 {
     class Program
     {
+        public struct Schedule
+        {
+            public int start;
+            public int end;
+
+            public Schedule(int start, int end)
+            {
+                this.start = start;
+                this.end = end;
+            }
+        }
+
         static void Main(string[] args)
         {
+            List<Schedule> timeTable = new List<Schedule>();
+            int count = int.Parse(Console.ReadLine());
+            for (int i = 0; i < count; i++)
+            {
+                var row = Array.ConvertAll(Console.ReadLine().Split(' '), e => int.Parse(e));
+                timeTable.Add(new Schedule(row[0], row[1]));
+            }
+            timeTable.Sort((a, b) => a.end == b.end ? a.start - b.start : a.end - b.end);
+
+            int curTime = 0;
+            int confCount = 0;
+            for (int i = 0; i < count; i++)
+            {
+                if (timeTable[i].start >= curTime)
+                {
+                    curTime = timeTable[i].end;
+                    confCount++;
+                }
+            }
+
+            Console.WriteLine(confCount);
         }
     }
 }
