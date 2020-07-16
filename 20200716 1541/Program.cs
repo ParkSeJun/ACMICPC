8e772d2e7cd40aa0863c07be0e1f48e4d3ea8747
ParkSeJun <chunsa1166@naver.com>
2020-07-16 15:10:43 +0900

    1541 잃어버린 괄호 정답 (편법)

---
+++ 20200716 1541/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace _20200716_1541
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var row = Console.ReadLine().Replace("+", " ").Replace("-", " -").Split(' ').Select(e => int.Parse(e)).ToList();
+
+            int sum = 0;
+            bool meetNegative = false;
+            for (int i = 0; i < row.Count; i++)
+            {
+                if (meetNegative)
+                    sum -= Math.Abs(row[i]);
+                else
+                {
+                    sum += row[i];
+                    if (row[i] < 0)
+                        meetNegative = true;
+                }
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
