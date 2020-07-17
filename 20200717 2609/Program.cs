9f16e0b902a8614ece5f461cd36463bf649b9b39
ParkSeJun <chunsa1166@naver.com>
2020-07-17 11:53:28 +0900

    2609 최대공약수와 최소공배수 정답.

---
+++ 20200717 2609/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _20200717_2609
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var row = Array.ConvertAll( Console.ReadLine().Split(' '), e => int.Parse(e));
+            int gcd = GCD(row[0], row[1]);
+            int lcm = LCM(row[0], row[1], gcd);
+            Console.WriteLine(gcd);
+            Console.WriteLine(lcm);
+        }
+
+        static int GCD(int a, int b)
+        {
+            if (b == 0)
+                return a;
+            return GCD(b, a % b);
+        }
+        static int LCM(int a, int b, int gcd)
+        {
+            return a / gcd * b;
+        }
+    }
+}
