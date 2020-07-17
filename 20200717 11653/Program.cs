d9e7c67499665e7fcfbd79c0c2a21bca33e199f9
ParkSeJun <chunsa1166@naver.com>
2020-07-17 11:44:43 +0900

    11653 소인수분해 정답

---
+++ 20200717 11653/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _20200717_11653
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 2; i <= n; i++)
+            {
+                while (n % i == 0)
+                {
+                    n /= i;
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
