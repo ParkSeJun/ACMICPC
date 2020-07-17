8de9121b42f53bbb894f359cff9cd6ebd4daf2e3
ParkSeJun <chunsa1166@naver.com>
2020-07-17 17:48:25 +0900

    11050 이항 계수 1 정답.

---
+++ 20200717 11050/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+/*
+5 2
+10
+*/
+namespace _20200717_11050
+{
+    class Program
+    {
+        static Dictionary<KeyValuePair<int, int>, int> table = new Dictionary<KeyValuePair<int, int>, int>();
+
+        static void Main(string[] args)
+        {
+            var row = Array.ConvertAll(Console.ReadLine().Split(), e => int.Parse(e));
+            int n = row[0];
+            int k = row[1];
+
+            Console.WriteLine( Solve(n, k));
+        }
+
+        static int Solve(int n, int k)
+        {
+            if (n == k || k == 0)
+                return 1;
+
+            var kvp = new KeyValuePair<int,int>(n, k);
+            if(!table.ContainsKey(kvp))
+                table[kvp] =  Solve(n - 1, k) + Solve(n - 1, k - 1);
+
+            return table[kvp];
+        }
+    }
+}
