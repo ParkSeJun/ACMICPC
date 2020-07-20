2cf1d7eda2fe6ebb8931a5252fadaae84e2dd1b3
ParkSeJun <chunsa1166@naver.com>
2020-07-20 14:28:00 +0900

    11051 정답

--- 20200717 11051/Program.cs
+++ 20200717 11051/Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Runtime.InteropServices;
 /*
 5 2
 10
@@ -14,12 +11,9 @@ namespace _20200717_11051
 
         static void Main(string[] args)
         {
-            //var row = Array.ConvertAll(Console.ReadLine().Split(' '), e => int.Parse(e));
-            //int n = row[0];
-            //int k = row[1];
-
-            int n = 1000;
-            int k = 500;
+            var row = Array.ConvertAll(Console.ReadLine().Split(' '), e => int.Parse(e));
+            int n = row[0];
+            int k = row[1];
 
             arr = new int[n + 1][];
             for (int i = 0; i < n + 1; i++)
@@ -36,7 +30,7 @@ namespace _20200717_11051
 
             for (int i = 1; i < n + 1; i++)
                 for (int j = 1; j < k + 1; j++)
-                    arr[i][j] = arr[i - 1][j] + arr[i - 1][j - 1];
+                    arr[i][j] = (arr[i - 1][j] + arr[i - 1][j - 1]) % 10007;
 
             Console.WriteLine(arr[n][k]);
         }
