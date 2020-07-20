3db185c76e677e22b26c3109ce7df22a1be4cace
ParkSeJun <chunsa1166@naver.com>
2020-07-20 15:06:59 +0900

    9375 정답

--- 20200720 9375/Program.cs
+++ 20200720 9375/Program.cs
@@ -1,8 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
+/*
+2
+3
+hat headgear
+sunglasses eyewear
+turban headgear
+3
+mask face
+sunglasses face
+makeup face
+
+    5
+    3
+*/
 
 namespace _20200720_9375
 {
@@ -10,6 +23,28 @@ namespace _20200720_9375
     {
         static void Main(string[] args)
         {
+            Dictionary<string, int> dic = new Dictionary<string, int>();
+            int caseCount = int.Parse(Console.ReadLine());
+
+            for (int @case = 0; @case < caseCount; @case++)
+            {
+                int count = int.Parse(Console.ReadLine());
+                for (int i = 0; i < count; i++)
+                {
+                    var row = Console.ReadLine().Split(' ');
+                    if (!dic.ContainsKey(row[1]))
+                        dic[row[1]] = 0;
+                    dic[row[1]]++;
+                }
+                var keys = dic.Keys.ToList();
+                int sum = 1;
+                for (int j = 0; j < keys.Count; j++)
+                    sum *= dic[keys[j]] + 1;
+                sum--;
+
+                Console.WriteLine(sum);
+                dic.Clear();
+            }            
         }
     }
 }
