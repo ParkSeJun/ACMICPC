3832bd5858622bb0b28df046e6253a18d49f4ff0
ParkSeJun <chunsa1166@naver.com>
2020-07-20 16:30:43 +0900

    1676 정답

--- 20200720 1676/Program.cs
+++ 20200720 1676/Program.cs
@@ -1,15 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _20200720_1676
 {
     class Program
     {
+        static Dictionary<int, int> div2 = new Dictionary<int, int>();
+        static Dictionary<int, int> div5 = new Dictionary<int, int>();
+
         static void Main(string[] args)
         {
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Solve(n));
+        }
+
+        static int Solve(int n)
+        {
+            int div2 = 0;
+            int div5 = 0;
+
+
+            for (int i = 2; i <= n; i++)
+            {
+                div2 += Get2(i);
+                div5 += Get5(i);
+            }
+
+            return Math.Min(div2, div5);
+        }
+
+        static int Get2(int n)
+        {
+            if (n < 2 || (n & 1) != 0)
+                return 0;
+
+            if (!div2.ContainsKey(n))
+                div2[n] = 1 + Get2(n / 2);
+
+            return div2[n];
+        }
+
+        static int Get5(int n)
+        {
+            if (n < 5 || n % 5 != 0)
+                return 0;
+
+            if (!div5.ContainsKey(n))
+                div5[n] = 1 + Get5(n / 5);
+
+            return div5[n];
         }
     }
 }
