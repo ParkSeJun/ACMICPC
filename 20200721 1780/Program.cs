b8fa0832927498f7767d310bf83b4e4d4ed6a440
ParkSeJun <chunsa1166@naver.com>
2020-07-21 18:27:29 +0900

    1780 정답

--- 20200721 1780/Program.cs
+++ 20200721 1780/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 /*
 9
@@ -29,14 +27,16 @@ namespace _20200721_1780
         {
             public int size;
             public int y, x;
-            Node[] link;
+            public Node[] link;
 
-            public Node(int size, int  y, int x)
+            public bool HasChild => link != null;
+            public int Color => arr[y][x];
+
+            public Node(int size, int y, int x)
             {
                 this.size = size;
                 this.y = y;
                 this.x = x;
-                link = new Node[9];
             }
         }
 
@@ -47,8 +47,75 @@ namespace _20200721_1780
             arr = new int[count][];
             for (int i = 0; i < count; i++)
             {
-
+                arr[i] = new int[count];
+                var row = Array.ConvertAll(Console.ReadLine().Split(' '), e => int.Parse(e));
+                for (int j = 0; j < count; j++)
+                    arr[i][j] = row[j];
             }
+
+            //var rand = new Random();
+            //int count = (int)Math.Pow(3, 7);
+            //arr = new int[count][];
+            //for (int i = 0; i < count; i++)
+            //{
+            //    arr[i] = new int[count];
+            //    for (int j = 0; j < count; j++)
+            //        arr[i][j] = j % 3 - 1;
+            //}
+
+
+            Node head = new Node(count, 0, 0);
+
+            int a = 0;
+            int b = 0;
+            int c = 0;
+
+            Stack<Node> stack = new Stack<Node>();
+            stack.Push(head);
+            while (stack.Count > 0)
+            {
+                Node node = stack.Pop();
+                if (IsOneColor(node))
+                {
+                    switch (node.Color)
+                    {
+                        case -1:
+                            a++;
+                            break;
+                        case 0:
+                            b++;
+                            break;
+                        case 1:
+                            c++;
+                            break;
+                    }
+                    continue;
+                }
+
+                node.link = new Node[9];
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        node.link[i * 3 + j] = new Node(node.size / 3, node.y + (node.size / 3) * i, node.x + (node.size / 3) * j);
+                        stack.Push(node.link[i * 3 + j]);
+                    }
+                }
+            }
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+        }
+
+        static bool IsOneColor(Node node)
+        {
+            int color = arr[node.y][node.x];
+            for (int i = 0; i < node.size; i++)
+                for (int j = 0; j < node.size; j++)
+                    if (arr[node.y + i][node.x + j] != color)
+                        return false;
+            return true;
         }
     }
 }
