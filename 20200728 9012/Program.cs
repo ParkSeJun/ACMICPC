6a70d5c6368b2a551fce90d71a75a10f0c0edc90
ParkSeJun <chunsa1166@naver.com>
2020-07-28 12:31:42 +0900

    9012 괄호 정답.

---
+++ 20200728 9012/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _20200728_9012
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int stack = 0;
+            int count = int.Parse(Console.ReadLine());
+            for (int i = 0; i < count; i++)
+            {
+                var row = Console.ReadLine();
+                stack = 0;
+                for (int j = 0; j < row.Length; j++)
+                {
+                    if (row[j] == '(')
+                        stack++;
+                    else if (row[j] == ')')
+                    {
+                        stack--;
+                        if (stack < 0)
+                            break;
+                    }
+                }
+                if (stack == 0)
+                    Console.WriteLine("YES");
+                else
+                    Console.WriteLine("NO");
+            }
+        }
+    }
+}
