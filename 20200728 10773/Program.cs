016ffda4250aafcc68a29bf09881bf2f09c8f111
ParkSeJun <chunsa1166@naver.com>
2020-07-28 12:20:49 +0900

    10773 제로 정답

---
+++ 20200728 10773/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _20200728_10773
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int>();
+            int count = int.Parse(Console.ReadLine());
+            for (int i = 0; i < count; i++)
+            {
+                int n = int.Parse(Console.ReadLine());
+                if (n == 0)
+                    stack.Pop();
+                else
+                    stack.Push(n);
+            }
+
+            Console.WriteLine(stack.Sum());
+        }
+    }
+}
