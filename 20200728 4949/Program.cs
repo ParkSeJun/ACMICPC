fb933ed68ffa6d345874d4d12e3c255ec65dd4c1
ParkSeJun <chunsa1166@naver.com>
2020-07-28 12:46:40 +0900

    4949 균형잡힌 세상 정답.

---
+++ 20200728 4949/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _20200728_4949
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<char> stack = new Stack<char>();
+            StringBuilder sb = new StringBuilder();
+            while (true)
+            {
+                var row = Console.ReadLine();
+                if (row.Equals("."))
+                    break;
+
+                try
+                {
+                    for (int j = 0; j < row.Length; j++)
+                    {
+                        switch (row[j])
+                        {
+                            case '(':
+                            case '[':
+                                stack.Push(row[j]);
+                                break;
+                            case ')':
+                                if (stack.Count == 0 || stack.Pop() != '(')
+                                    throw null;
+                                break;
+                            case ']':
+                                if (stack.Count == 0 || stack.Pop() != '[')
+                                    throw null;
+                                break;
+                        }
+                    }
+                    if (stack.Count != 0)
+                        throw null;
+
+                    sb.AppendLine("yes");
+                }
+                catch
+                {
+                    sb.AppendLine("no");
+                }
+
+                stack.Clear();
+            }
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
