fac6cd972b0868feae2887ff0fbbbbf61e764a86
ParkSeJun <chunsa1166@naver.com>
2020-07-29 18:10:34 +0900

    2740 행렬 곱셈 정답.

---
+++ 20200729 2740/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace _20200729_2740
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, m, k;
+            int[][] arr1;
+            int[][] arr2;
+
+            var row = Array.ConvertAll(Console.ReadLine().Split(' '), e => int.Parse(e));
+            n = row[0];
+            m = row[1];
+
+            arr1 = new int[n][];
+            for (int i = 0; i < n; i++)
+            {
+                arr1[i] = new int[m];
+                row = Array.ConvertAll(Console.ReadLine().Split(' '), e => int.Parse(e));
+                for (int j = 0; j < m; j++)
+                    arr1[i][j] = row[j];
+            }
+
+            row = Array.ConvertAll(Console.ReadLine().Split(' '), e => int.Parse(e));
+            m = row[0];
+            k = row[1];
+
+            arr2 = new int[m][];
+            for (int i = 0; i < m; i++)
+            {
+                arr2[i] = new int[k];
+                row = Array.ConvertAll(Console.ReadLine().Split(' '), e => int.Parse(e));
+                for (int j = 0; j < k; j++)
+                    arr2[i][j] = row[j];
+            }
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < k; j++)
+                {
+                    int sum = 0;
+                    for (int a = 0; a < m; a++)
+                        sum += arr1[i][a] * arr2[a][j];
+                    sb.Append(sum.ToString());
+                    sb.Append(" ");
+                }
+                sb.AppendLine();
+            }
+
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
