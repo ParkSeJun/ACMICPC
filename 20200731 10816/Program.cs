fd1aec3468568c39d08edf8e3f670e21fe1e31f2
ParkSeJun <chunsa1166@naver.com>
2020-07-31 17:28:12 +0900

    10816, 1654 도중 커밋.

---
+++ 20200731 10816/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _20200731_10816
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var row = Array.ConvertAll(Console.ReadLine().Split(' '), e => int.Parse(e));
+            List<int> l = new List<int>();
+            for (int i = 0; i < n; i++)
+                l.Add(row[i]);
+            l.Sort();
+
+            n = int.Parse(Console.ReadLine());
+            row = Array.ConvertAll(Console.ReadLine().Split(' '), e => int.Parse(e));
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < n; i++)
+            {
+                sb.Append(GetCount(l, row[i]));
+                sb.Append(" ");
+            }
+            Console.WriteLine(sb.ToString());
+        }
+
+        static int GetCount(List<int> l, int n)
+        {
+            int count = 0;
+
+            int s = 0;
+            int e = l.Count - 1;
+            int findIndex = -1;
+            while (s <= e)
+            {
+                int mid = (s + e) / 2;
+                if (l[mid] == n)
+                {
+                    findIndex = mid;
+                    break;
+                }
+                if (l[mid] < n)
+                    s = mid + 1;
+                else
+                    e = mid - 1;
+            }
+
+            if (findIndex == -1)
+                return 0;
+
+
+
+
+
+            return count;
+        }
+    }
+}
---
+++ 20200731 1654/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+
+/*
+4 11
+802
+743
+457
+539
+
+*/
+
+namespace _20200731_1654
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var row = Array.ConvertAll(Console.ReadLine().Split(' '), e_ => int.Parse(e_));
+            int c = row[0];
+            int n = row[1];
+
+            List<int> l = new List<int>();
+            for (int i = 0; i < c; i++)
+                l.Add(int.Parse(Console.ReadLine()));
+            l.Sort();
+
+            int s = 0;
+            int e = l[l.Count - 1];
+            int findN_1 = -1;
+            int findN = -1;
+            while (true)
+            {
+                int mid = (s + e) / 2;
+
+                int sum = 0;
+                for (int i = 0; i < l.Count; i++)
+                    sum += l[i] / mid;
+
+                if (sum == n)
+                {
+                    findN = mid;
+                    if (findN != -1 && findN_1 != -1)
+                        break;
+                    s = mid + 1;
+                }
+                else if (sum == n - 1)
+                {
+                    findN_1 = mid;
+                    if (findN != -1 && findN_1 != -1)
+                        break;
+                    e = mid - 1;
+                }
+                else if (sum < n)
+                    e = mid - 1;
+                else
+                    s = mid + 1;
+            }
+
+            s = findN;
+            e = findN_1;
+            while (true)
+            {
+                if (s == e - 1)
+                    break;
+
+                int mid = (s + e) / 2;
+
+                int sum = 0;
+                for (int i = 0; i < l.Count; i++)
+                    sum += l[i] / mid;
+
+                if (sum == n)
+                    s = mid;
+                else if (sum == n - 1)
+                    e = mid;
+            }
+
+            Console.WriteLine(s);
+        }
+    }
+}
